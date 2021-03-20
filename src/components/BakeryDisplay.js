a0aebcf95061c30c3a881acf0b243a86b000bdac
chsohn15 <chsohn15@gmail.com>
2021-03-20 14:20:43 -0400

    add fetch request

--- src/components/BakeryDisplay.js
+++ src/components/BakeryDisplay.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function BakeryDisplay(){
+    const [bakeries, setBakeries] = useState([]);
+    
+    useEffect( () => {
+        fetch("https://cupcake-bakery.herokuapp.com/stores")
+        .then(res => res.json())
+        .then(bakeries => console.log(bakeries))
+      });
+
     return(
         <div>
             Display bakeries here
