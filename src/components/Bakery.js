8cc8e430ce79129ddb235b187c79b0d5fa98154e
chsohn15 <chsohn15@gmail.com>
2021-03-23 19:20:05 -0400

    add react router dom

--- src/components/Bakery.js
+++ src/components/Bakery.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 function Bakery(props){
     
-    let {name, location} = props.bakery
+    let {name, location, desserts} = props.bakery
+
+    let handleClick = ()=> {
+        console.log(desserts)
+    }
+
     return(
         <div>
-           <h3>{name}</h3>
+           <h3 onClick={handleClick}>{name}</h3>
            <div>{location}</div>
         </div>
     )
 }
 
 export default Bakery; 
+
+//https://github.com/chsohn15/hp_writing_app_frontend/blob/master/src/components/userPage/CompletedActivityLink.js
