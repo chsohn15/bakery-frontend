89bc652970e420a492e5059ffe72726506250744
chsohn15 <chsohn15@gmail.com>
2021-03-30 20:47:43 -0400

    dessert display component

--- src/components/BakerySite/BakerySite.js
+++ src/components/BakerySite/BakerySite.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import BakeryName from './BakeryName.js'
+import DessertDisplay from './DessertDisplay.js'
 
 function BakerySite({match, location}){
-    
-    useEffect( () => {
+    // Maybe use fetch instead of props later
+    //useEffect( () => {
         const {id, name, desserts} = location.bakery
         const storeLocation = location.bakery.location
-        console.log(id, name, desserts)
-      });
+        //console.log(id, name, desserts)
+      //});
 
     return(
         <div>
            This is one bakery's info page
            <BakeryName bakery={location.bakery}/>
+           <DessertDisplay desserts={desserts}/>
         </div>
     )
 }
---
+++ src/components/BakerySite/DessertDisplay.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+function DessertDisplay(props){
+    const desserts = props.desserts
+
+    return(
+        <div>
+           Display desserts here
+        </div>
+    )
+}
+
+export default DessertDisplay; 
